a131f888179178c6e153191ef2cf7d7b39db2737
Kamal Ahmad <kamaal321@icloud.com>
2021-06-07 18:43:40 +0000

    added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,12 +1,9 @@
 module "network" {
   
   resource_group_name = "${var.prefix}-workshop"
-  address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+  
 
  source  = "app.terraform.io/kamaal-training/network/azurerm"
   version = "3.0.1"
   # insert required variables here
-}
 }
